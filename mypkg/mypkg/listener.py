54bc93d270129ac989377e1531f2b7d3bdb104ab
yukihiro <s21c1059ya@s.chibakoudai.jp>
2022-11-24 14:22:02 +0900

    Initial Commit

---
+++ mypkg/mypkg/listener.py
@@ -0,0 +1,14 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.msg import Person 
+
+def cb(msg):
+    global node
+
+    node.get_logger().info("Listen: %s" % msg)
+
+rclpy.init()
+node = Node("listener")
+pub = node.create_subscription(Person, "person", cb, 10)
+
+rclpy.spin(node)
---
+++ mypkg/mypkg/talker.py
@@ -0,0 +1,22 @@
+import rclpy
+from rclpy.node import Node
+
+
+
+from person_msgs.msg import Person
+
+rclpy.init()
+node = Node("talker")
+pub = node.create_publisher(Person, "person", 10)
+n = 0 
+
+def cb():
+    global n
+    msg = Person()
+    msg.name = "塩島徹大" 
+    msg.age = n
+    pub.publish(msg)
+    n += 1
+
+node.create_timer(0.5, cb)
+rclpy.spin(node)
